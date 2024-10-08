4d383dbd78c8b959204118f5efeb823aec1990bc
AizenSousuke <kurono1807@gmail.com>
2024-10-08 03:42:54 -0500

    added responsive contact component

--- src/app/contact/index.tsx
+++ src/app/contact/index.tsx
@@ -24,8 +24,8 @@ const Contact = ({ id, dictionary }: Props) => {
     },
   ];
   return (
-    <section id={id} className="pt-[100px] pb-[50px]">
-      <h3 className="font-fondamento uppercase tracking-[3.5px] text-[32.5px] text-center text-sky-blue-200">
+    <section id={id} className="pt-[75px] sm:pt-[100px] pb-[50px]">
+      <h3 className="font-fondamento uppercase tracking-[3.5px] text-xl sm:text-3xl text-center text-sky-blue-200">
         {dictionary?.title}
       </h3>
       <LineXStyle className="my-10" />
@@ -38,7 +38,7 @@ const Contact = ({ id, dictionary }: Props) => {
         <div>
           <label
             htmlFor="email"
-            className="text-[20px] tracking-[3px] font-fondamento text-black-200"
+            className="text-base sm:text-xl tracking-[3px] font-fondamento text-black-200"
           >
             {dictionary?.form?.email?.name}:
           </label>
@@ -48,13 +48,13 @@ const Contact = ({ id, dictionary }: Props) => {
             placeholder={dictionary?.form?.email?.name}
             name="email"
             required
-            className="rounded-[5px] cursor-pointer border-none text-lg font-medium my-[15px] p-2.5 w-[75%] bg-whites-100 h-10"
+            className="rounded-[5px] cursor-pointer border-none text-base sm:text-lg font-medium my-[15px] p-2.5 w-[85%] sm:w-[75%] bg-whites-100 h-10"
           ></input>
         </div>
         <div>
           <label
             htmlFor="mensaje"
-            className="text-[20px] text-black-200 tracking-[3px] font-fondamento"
+            className="text-base sm:text-xl text-black-200 tracking-[3px] font-fondamento"
           >
             {dictionary?.form?.message?.name}:
           </label>
@@ -65,11 +65,11 @@ const Contact = ({ id, dictionary }: Props) => {
             rows={5}
             cols={24}
             required
-            className="border-none rounded-[5px] cursor-pointer text-lg font-medium my-[15px] p-2.5 w-[75%] bg-whites-100"
+            className="border-none rounded-[5px] cursor-pointer text-base sm:text-lg font-medium my-[15px] p-2.5 w-[85%] sm:w-[75%] bg-whites-100"
           />
         </div>
         <button
-          className="bg-black-100 shadow-[0px_0px_10px_1px_skyblue] rounded-[15px] text-white tracking-widest border-none cursor-pointer text-[20px] font-semibold mx-0 my-10 p-[12.5px] bg-[linear-gradient(to_right,transparent,transparent),linear-gradient(to_right,#4e89a7,#4e89a7)]
+          className="bg-black-100 shadow-[0px_0px_10px_1px_skyblue] rounded-[15px] text-white tracking-widest border-none cursor-pointer text-lg sm:text-xl font-semibold mx-0 my-6 sm:my-10 p-[12.5px] bg-[linear-gradient(to_right,transparent,transparent),linear-gradient(to_right,#4e89a7,#4e89a7)]
           bg-[position:0px_0px] bg-no-repeat bg-[length:0%_100%] hover:bg-[length:100%_100%] hover:scale-[1.02] transition-all duration-300
           "
           type="submit"
@@ -77,7 +77,7 @@ const Contact = ({ id, dictionary }: Props) => {
           {dictionary?.form?.button?.name}
         </button>
       </form>
-      <div className="flex justify-center gap-28 overflow-hidden pt-6 pb-2">
+      <div className="flex justify-around sm:justify-center gap-0 sm:gap-28 overflow-hidden pt-6 pb-2">
         {social_networks &&
           social_networks.map((s_network, i) => (
             <a
@@ -87,7 +87,7 @@ const Contact = ({ id, dictionary }: Props) => {
               rel="noopener noreferrer"
             >
               <img
-                className="w-10 h-10 hover:opacity-70 hover:scale-[1.05] transition-all duration-300 cursor-pointer"
+                className="w-9 sm:w-10 h-9 sm:h-10 hover:opacity-70 hover:scale-[1.05] transition-all duration-300 cursor-pointer"
                 src={`icons/social/${s_network?.name}.svg`}
                 alt={s_network?.name}
               />
